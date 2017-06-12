b0233184ecd2d125d617f49093decc0d283d236f
RS <semak.roman@gmail.com>
2017-06-13 00:08:52 +0300

    product content

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,10 @@
 $(function() {
-
+	
+	//slider
+	$('.slider__nav__btn').click(function(){
+		$(this).closest('.slider__nav').find('button').toggleClass('_active');
+		$(this).closest('.slider').find('.slider__item').slideToggle();
+	})
+	
+	
 });
