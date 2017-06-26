d70cf05dd7d8d4e161efd1a78cbd259ab0d2d237
RS <semak.roman@gmail.com>
2017-06-26 23:09:32 +0300

    product changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,32 +1,50 @@
-$(function() {
-	
+$(function () {
+	$(".smooth-scroll").on("click", function (e) {
+		e.preventDefault();
+		var id = $(this).attr('href');
+		var top = $(id).offset().top;
+		$('body,html').animate({
+			scrollTop: top
+		}, 1500);
+	});
+
 	//menu
-	$('.menu-btn').click(function(){
+	$('.menu-btn').click(function () {
 		$('nav').addClass('_show')
 		$('.menu-btn').fadeOut();
 	})
-	$('.nav__close').click(function(){
+	$('.nav__close').click(function () {
 		$('nav').removeClass('_show');
 		$('.menu-btn').fadeIn();
 	})
-	
+
+	//cat paw
+	$('.rules__button')
+		.mouseover(function () {
+			$('.rules-cat-leg').addClass('paw-anim');
+		})
+		.mouseout(function () {
+			$('.rules-cat-leg').removeClass('paw-anim');
+		});
+
 	//slider
-	$('.slider__nav__btn').click(function(){
+	$('.slider__nav__btn').click(function () {
 		$(this).closest('.slider__nav').find('button').toggleClass('_active');
 		$(this).closest('.slider').find('.slider__item').fadeToggle();
 	})
-	
+
 	//prod show
-	$('.prod__link._green').click(function(e){
+	$('.prod__link._green').click(function (e) {
 		e.preventDefault();
 		$(this).closest('section').find('.prod-hide').fadeToggle();
 	})
-	
+
 	//game
-//	$('.spotlight').spotlight();
-	$('.game-btn, ._room').click(function(){
+	//	$('.spotlight').spotlight();
+	$('.game-btn, ._room').click(function () {
 		$(this).closest('.game__window').hide().next('.game__window').fadeIn();
 	})
-	
-	
+
+
+
 });
